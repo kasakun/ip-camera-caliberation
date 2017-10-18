3d7bde1780c1666554a62cc2d3e29943260f47e2
Zeyu Chen <chenzy@gatech.edu>
2017-10-18 01:18:37 -0400

    Add calibration part

---
+++ calibration/CMakeFiles/CameraCalibration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CameraCalibration.dir/CameraCalibration.cpp.o"
+  "CameraCalibration.pdb"
+  "CameraCalibration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CameraCalibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
